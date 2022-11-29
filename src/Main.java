d121a20e8525428d77bc2f10350910a2267e0fc1
Boqueronix <jgonseb@gmail.com>
2022-11-29 00:20:13 -0500

    Removing tests

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,11 @@ public class Main {
     public static Card[][] ordered = new Card[4][13];
     public static Card[] deck = new Card[52];
     public static void main(String[] args) {
-        //System.out.println("IK how to github");
         Board.init();
         for (int i = 0; i < 52; i++) {
             ordered[i / 13][i % 13] = new Card(i / 13, (i % 13) + 1);
         }
-//        for (int i = 0; i < 52; i++) {
-//            System.out.println(ordered[i / 13][i % 13]);
-//        }
         shuffle();
-        for (int i = 0; i < 52; i++) {
-            System.out.println(deck[i]);
-        }
     }
     public static void shuffle(){
         int[] used = new int[52];
