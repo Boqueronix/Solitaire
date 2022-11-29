1fc7b996d6756c96dcdd00b03ceac2324aa6a9c5
Boqueronix <jgonseb@gmail.com>
2022-11-28 23:15:00 -0500

    Initializing and filling arrays

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+    public static Card[][] ordered = new Card[4][13];
+    public static Card[] deck = new Card[52];
     public static void main(String[] args) {
-        System.out.println("IK how to github");
+        //System.out.println("IK how to github");
+        Board.init();
+        for (int i = 0; i < 52; i++) {
+            ordered[i / 13][i % 13] = new Card(i / 13, (i % 13) + 1);
+        }
+        for (int i = 0; i < 52; i++) {
+            System.out.println(ordered[i / 13][i % 13]);
+        }
+
     }
 }
